440303e480043912b2b23d7a0ebfadf6badbef04
Franco Vallejos Vigier <francovallejosvigier@gmail.com>
2023-10-31 23:51:59 -0300

    listo ekf falta testear. izi piz

--- ekf.py
+++ ekf.py
@@ -32,10 +32,26 @@ class ExtendedKalmanFilter:
         next_sigma = (Gt@self.sigma@Gt.T) + (Vt@Mt@Vt.T)
         return next_mu,next_sigma
 
-    def _correction_step(self, env, predicted_mu, predicted_sigma):
+    def _correction_step(self, env, predicted_not_corrected_mu, predicted_not_corrected_sigma,
+                         zt, features_observados_con_id):
         #________________Correction Step____________________
-        # TO DO
-        return predicted_mu, predicted_sigma
+        predicted_corrected_carry_mu = predicted_not_corrected_mu
+        predicted_corrected_carry_sigma = predicted_not_corrected_sigma
+                             
+        Qt = self.beta
+        for (marker_id, landmark_observado) in features_observados_con_id:
+            predicted_mu_x, predicted_mu_y, predicted_mu_theta = predicted_corrected_carry_mu.ravel()
+            j = marker_id
+            q = (env.MARKER_X_POS[j] - predicted_mu_x)**2 + (env.MARKER_Y_POS[j] - predicted_mu_y)**2
+            z_piquito = np.array([[np.sqrt([q])[0], minimized_angle(np.arctan2([env.MARKER_Y_POS[j] - predicted_mu_y],
+                                                                               [env.MARKER_X_POS[j] - predicted_mu_x])[0] - predicted_mu_theta)]])
+            Ht = env.H(predicted_corrected_carry_mu,j)
+            St = Ht@predicted_corrected_carry_sigma@Ht.T + Qt
+            Kt = (predicted_corrected_carry_sigma@H.T)@np.linalg.pinv(St)
+            predicted_corrected_carry_mu = predicted_corrected_carry_mu + Kt@(zt - z_piquito)
+            predicted_corrected_carry_sigma = (np.eye(3) - Kt@Ht) @predicted_corrected_carry_sigma
+
+        return predicted_corrected_carry_mu, predicted_corrected_carry_sigma
 
     def update(self, env, u, z, marker_id):
         """Update the state estimate after taking an action and receiving a landmark
@@ -45,6 +61,6 @@ class ExtendedKalmanFilter:
         z: landmark observation
         marker_id: landmark ID ("Feature observado", cantidad 1)
         """
-        next_mu, next_sigma = self._prediction_step(env,u)  # Predicted mu and sigma 
-        self.mu, self.sigma = self._correction_step(env,next_mu, next_sigma)  # Correction over predicted mu and sigma 
+        next_mu, next_sigma = self._prediction_step(env,u)  # Predicted mu and sigma (not corrected yet)
+        self.mu, self.sigma = self._correction_step(env, next_mu, next_sigma, z, [(marker_id,z)])  # Correction over predicted mu and sigma 
         return self.mu, self.sigma
