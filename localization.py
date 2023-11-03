9125d9ca377d37ff4458060c020986882881f4ce
EmmanuelMess <emmanuelbendavid@gmail.com>
2023-11-03 12:36:47 -0300

    Plotear

--- localization.py
+++ localization.py
@@ -75,7 +75,7 @@ def localize(env, policy, filt, x0, num_steps, plot=False):
     if plot:
         plt.show(block=True)
 
-    return position_errors
+    return mean_position_error, mean_mahalanobis_error, anees
 
 
 def setup_parser():
@@ -103,22 +103,18 @@ def setup_parser():
     parser.add_argument(
         '--num-particles', type=int, default=100,
         help='number of particles (particle filter only)')
+    parser.add_argument(
+        '--plot-error', action='store_true',
+        help='plot error varying alpha and beta')
 
     return parser
 
 
-if __name__ == '__main__':
-    args = setup_parser().parse_args()
-    print('Data factor:', args.data_factor)
-    print('Filter factor:', args.filter_factor)
+def start(data_factor, filter_factor, num_particles, plot, args):
+    alphas = np.array([0.05 ** 2, 0.005 ** 2, 0.1 ** 2, 0.01 ** 2])
+    beta = np.diag([np.deg2rad(5) ** 2])
 
-    if args.seed is not None:
-        np.random.seed(args.seed)
-
-    alphas = np.array([0.05**2, 0.005**2, 0.1**2, 0.01**2])
-    beta = np.diag([np.deg2rad(5)**2])
-
-    env = Field(args.data_factor * alphas, args.data_factor * beta)
+    env = Field(data_factor * alphas, data_factor * beta)
     policy = policies.OpenLoopRectanglePolicy()
 
     initial_mean = np.array([180, 50, 0]).reshape((-1, 1))
@@ -130,17 +126,126 @@ if __name__ == '__main__':
         filt = ExtendedKalmanFilter(
             initial_mean,
             initial_cov,
-            args.filter_factor * alphas,
-            args.filter_factor * beta
+            filter_factor * alphas,
+            filter_factor * beta
         )
     elif args.filter_type == 'pf':
         filt = ParticleFilter(
             initial_mean,
             initial_cov,
-            args.num_particles,
-            args.filter_factor * alphas,
-            args.filter_factor * beta
+            num_particles,
+            filter_factor * alphas,
+            filter_factor * beta
         )
 
     # You may want to edit this line to run multiple localization experiments.
-    localize(env, policy, filt, initial_mean, args.num_steps, args.plot)
+    return localize(env, policy, filt, initial_mean, args.num_steps, plot)
+
+
+if __name__ == '__main__':
+    args = setup_parser().parse_args()
+
+    if args.seed is not None:
+        np.random.seed(args.seed)
+
+    if not args.plot_error:
+        print('Data factor:', args.data_factor)
+        print('Filter factor:', args.filter_factor)
+
+        _ = start(args.data_factor, args.filter_factor, args.num_particles, args.plot, args)
+    else:
+        if args.plot:
+            print("--plot ignored!")
+
+        AVERAGING_RUNS = 1
+        RS = [1 / 64, 1 / 16, 1 / 4, 4, 16, 64]
+        PARTICLES = [20, 50, 500]
+
+        if args.filter_type == 'pf':
+            mean_position_errors = [[] for _ in PARTICLES]
+            mean_mahalanobis_errors = [[] for _ in PARTICLES]
+            aneess = [[] for _ in PARTICLES]
+
+            for r in RS:
+                for i, num_particles in enumerate(PARTICLES):
+                    mean_position_runs = []
+                    mean_mahalanobis_runs = []
+                    anees_runs = []
+
+                    for _ in range(AVERAGING_RUNS):
+                        mean_position_error, mean_mahalanobis_error, anees = start(r, r, num_particles, False, args)
+
+                        mean_position_runs.append(mean_position_error)
+                        mean_mahalanobis_runs.append(mean_mahalanobis_error)
+                        anees_runs.append(anees)
+
+                    mean_position_errors[i].append(np.mean(mean_position_runs))
+                    mean_mahalanobis_errors[i].append(np.mean(mean_mahalanobis_runs))
+                    aneess[i].append(np.mean(anees_runs))
+
+            class _:
+                plt.title("Error de posicion medio sobre valores de r")
+                plt.xlabel('r')
+                plt.ylabel('Error')
+                plt.gca().set_xscale('log')
+                plt.gca().set_yscale('log')
+                for i, y in enumerate(mean_position_errors):
+                    plt.plot(RS, y, label=f"Error de posicion medio, {PARTICLES[i]} particulas")
+                plt.savefig('plots/pf-a.png')
+                plt.clf()
+
+                plt.title("Error de posicion medio y ANEES sobre valores de r")
+                plt.xlabel('r')
+                plt.ylabel('Error')
+                plt.gca().set_xscale('log')
+                plt.gca().set_yscale('log')
+                for i, y in enumerate(mean_position_errors):
+                    plt.plot(RS, y, label=f"Error de posicion medio, {PARTICLES[i]} particulas")
+                for i, y in enumerate(aneess):
+                    plt.plot(RS, y, label=f"ANEES, {PARTICLES[i]} particulas")
+                plt.legend()
+                plt.savefig('plots/pf-b.png')
+                plt.clf()
+
+        else:
+            mean_position_errors = []
+            mean_mahalanobis_errors = []
+            aneess = []
+            for r in RS:
+                mean_position_runs = []
+                mean_mahalanobis_runs = []
+                anees_runs = []
+
+                for _ in range(AVERAGING_RUNS):
+                    mean_position_error, mean_mahalanobis_error, anees = start(r, r, None, False, args)
+
+                    mean_position_runs.append(mean_position_error)
+                    mean_mahalanobis_runs.append(mean_mahalanobis_error)
+                    anees_runs.append(anees)
+
+                mean_position_errors.append(np.mean(mean_position_runs))
+                mean_mahalanobis_errors.append(np.mean(mean_mahalanobis_runs))
+                aneess.append(np.mean(anees_runs))
+
+            class _:
+                plt.title("Error de posicion medio sobre valores de r")
+                plt.xlabel('r')
+                plt.ylabel('Error')
+                plt.gca().set_xscale('log')
+                plt.gca().set_yscale('log')
+                plt.plot(RS, mean_position_errors)
+                plt.savefig('plots/ekf-a.png')
+                plt.clf()
+
+                plt.title("Error de posicion medio y ANEES sobre valores de r")
+                plt.xlabel('r')
+                plt.ylabel('Error')
+                plt.gca().set_xscale('log')
+                plt.gca().set_yscale('log')
+                plt.plot(RS, mean_position_errors, label="Error de posicion medio")
+                plt.plot(RS, aneess, label="ANEES")
+                plt.legend()
+                plt.savefig('plots/ekf-b.png')
+                plt.clf()
+
+
